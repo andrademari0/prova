b97df2acbfca3685fdb2f36500334dc5995c6fa3
andrademari0 <marianasilvaandrade809@gmail.com>
2024-09-05 18:33:21 -0300

    Add files via upload

---
+++ provaa.sql
@@ -0,0 +1,33 @@
+CREATE DATABASE prova;
+
+CREATE TABLE prova 
+
+questão2
+INSERT INTO projetos(nomes_começaram_apos_01_março),
+(1,ana costa )
+(2,lucas almeida)
+
+SELECT 
+    nome.funcionarios,
+    nome.nomes
+    funcionarios.nomes
+FROM nome
+INNER JOIN funcionarios ON nome.funcionarios.nomes= funcionarios.funcionarios <1_março
+
+
+questão 4 
+INSERT INTO tabela (nome_fucionarios, salrio_superior,6000)
+(1, maria oliveira)
+(2,carlos pereira)
+(3,lucas almeida)
+
+SELECT 
+	salarios.salario
+    salarios.valor
+    valor.nome AS nome_valor
+FROM salarios 
+INNER JOIN valor ON salarios.valor_id= salarios.salario superior a 6000;
+
+
+
+
